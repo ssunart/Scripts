9d98796cc158fe80c052a5a8aa11bffa3a22d91f
Soowon Lee <136606810+ssunart@users.noreply.github.com>
2024-12-06 11:15:33 +0900

    Update run_searchProt.sh

--- Template_search/ProBis/run_searchProt.sh
+++ Template_search/ProBis/run_searchProt.sh
@@ -5,12 +5,21 @@
 shopt -s expand_aliases
 alias probis='/home/soowon/apps/probis/probis'
 
-TARGET=TARGET/
+TARGET=TARGET/8sya_prot
 probis -extract -f1 ${TARGET}.pdb -c1 A -srffile ${TARGET}.srf
 
 cd protein/
+
+
+start_time=$(date +%s)
+echo "Start processing at $(date)"    
 
 probis -ncpu 2 -surfdb -local -sfile prot_srf.txt -f1 ../${TARGET}.srf -c1 A -nosql example.nosql
 
 probis -results -f1 ../${TARGET}.pdb -c1 A -nosql example.nosql -json example.json
 python check_json.py
+
+
+
+end_time=$(date +%s)
+echo finished at $(date)
