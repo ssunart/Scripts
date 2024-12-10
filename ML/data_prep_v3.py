05d2cbbe8bcfc91d37b3da905c7f12d750adffb4
Soowon Lee <136606810+ssunart@users.noreply.github.com>
2024-12-10 10:56:06 +0900

    Create data_prep_v3.py

---
+++ ML/data_prep_v3.py
@@ -0,0 +1,354 @@
+###원시 데이터(merged_300K.csv)에서 SMILES와 점수를 정리하여 새로운 파일(cleaned.csv)을 생성한 뒤 작업을 시작
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+##Module load
+
+
+# In[25]:
+
+
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+
+import rdkit
+import rdkit.Chem as Chem
+import rdkit.Chem.AllChem as AllChem
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import sklearn
+from tqdm import tqdm # to show a progress bar.
+
+# In[31]:
+
+
+df = pd.read_csv("cleaned.csv")
+##pd.read_csv("merged_300K.csv")
+
+
+# In[32]:
+
+
+##MACCS 키 추가하기
+from rdkit import Chem
+from rdkit.Chem import MACCSkeys
+
+# MACCS 키를 생성하는 함수를 정의합니다.
+def generate_MACCS(smiles):
+    molecule = Chem.MolFromSmiles(smiles)
+    maccs_key = MACCSkeys.GenMACCSKeys(molecule)
+    return maccs_key.ToBitString()
+
+# 'SMILES' 열에 있는 각 SMILES 문자열에 대해 MACCS 키를 생성하고,
+# 이를 'MACCS'라는 새로운 열에 추가합니다.
+df['MACCS'] = df['SMILES'].apply(generate_MACCS)
+
+
+# In[33]:
+
+
+maccs_df=df["MACCS"]
+maccs_df ##check maccs data
+score_df=df["AD_GPU"]
+
+
+# In[34]:
+
+
+##ECFP 추가하기
+mols = [Chem.MolFromSmiles(x) for x in df["SMILES"]]
+
+radius = 2; nbits = 2048 # radius=1 ECFP2 / radius=2 ECFP4
+fp_list = []
+for m in tqdm(mols):
+  fp = AllChem.GetMorganFingerprintAsBitVect(m, radius, nBits=nbits) # if radius = 2, then fp is an ECFP4
+  fp_list.append(fp.ToList())
+
+
+# In[ ]:
+
+
+fp_df = pd.DataFrame(fp_list)
+fp_df ##Data check
+
+
+# In[23]:
+
+
+##RDKit descriptor 생성import numpy as np
+from rdkit.Chem import Descriptors
+from rdkit.Chem import rdMolDescriptors
+from rdkit import DataStructs
+from rdkit.Chem import MolFromSmiles
+from rdkit.Chem.GraphDescriptors import (BalabanJ, BertzCT, Chi0, Chi0n, Chi0v, Chi1,
+                                         Chi1n, Chi1v, Chi2n, Chi2v, Chi3n, Chi3v, Chi4n, Chi4v,
+                                         HallKierAlpha, Ipc, Kappa1, Kappa2, Kappa3)
+
+from rdkit.Chem.EState.EState_VSA import (EState_VSA1, EState_VSA10, EState_VSA11, EState_VSA2, EState_VSA3,
+                                          EState_VSA4, EState_VSA5, EState_VSA6, EState_VSA7, EState_VSA8, EState_VSA9,
+                                          VSA_EState1, VSA_EState10, VSA_EState2, VSA_EState3, VSA_EState4, VSA_EState5,
+                                          VSA_EState6, VSA_EState7, VSA_EState8, VSA_EState9,)
+
+from rdkit.Chem.Descriptors import (ExactMolWt, MolWt, HeavyAtomMolWt, MaxAbsPartialCharge, MinPartialCharge,
+                                    MaxPartialCharge, MinAbsPartialCharge, NumRadicalElectrons, NumValenceElectrons)
+from rdkit.Chem.EState.EState import (MaxAbsEStateIndex, MaxEStateIndex, MinAbsEStateIndex, MinEStateIndex,)
+from rdkit.Chem.Lipinski import (FractionCSP3, HeavyAtomCount, NHOHCount, NOCount, NumAliphaticCarbocycles,
+                                 NumAliphaticHeterocycles, NumAliphaticRings, NumAromaticCarbocycles, NumAromaticHeterocycles,
+                                 NumAromaticRings, NumHAcceptors, NumHDonors, NumHeteroatoms, RingCount,
+                                 NumRotatableBonds, NumSaturatedCarbocycles, NumSaturatedHeterocycles, NumSaturatedRings,)
+from rdkit.Chem.Crippen import (MolLogP, MolMR,)
+from rdkit.Chem.MolSurf import (LabuteASA, PEOE_VSA1, PEOE_VSA10, PEOE_VSA11, PEOE_VSA12, PEOE_VSA13, PEOE_VSA14,
+                                PEOE_VSA2, PEOE_VSA3,PEOE_VSA4, PEOE_VSA5, PEOE_VSA6, PEOE_VSA7, PEOE_VSA8, PEOE_VSA9,
+                                SMR_VSA1, SMR_VSA10, SMR_VSA2, SMR_VSA3, SMR_VSA4, SMR_VSA5, SMR_VSA6,
+                                SMR_VSA7, SMR_VSA8, SMR_VSA9, SlogP_VSA1, SlogP_VSA10, SlogP_VSA11, SlogP_VSA12,
+                                SlogP_VSA2, SlogP_VSA3,SlogP_VSA4, SlogP_VSA5, SlogP_VSA6, SlogP_VSA7, SlogP_VSA8,
+                                SlogP_VSA9, TPSA,)
+from rdkit.Chem.Fragments import (fr_Al_COO, fr_Al_OH, fr_Al_OH_noTert, fr_ArN, fr_Ar_COO, fr_Ar_N, fr_Ar_NH,
+ fr_Ar_OH, fr_COO, fr_COO2, fr_C_O, fr_C_O_noCOO, fr_C_S, fr_HOCCN, fr_Imine, fr_NH0, fr_NH1,
+ fr_NH2, fr_N_O, fr_Ndealkylation1, fr_Ndealkylation2, fr_Nhpyrrole, fr_SH, fr_aldehyde, fr_alkyl_carbamate,
+ fr_alkyl_halide, fr_allylic_oxid, fr_amide, fr_amidine, fr_aniline, fr_aryl_methyl, fr_azide, fr_azo, fr_barbitur,
+ fr_benzene, fr_benzodiazepine, fr_bicyclic, fr_diazo, fr_dihydropyridine, fr_epoxide, fr_ester, fr_ether, fr_furan,
+ fr_guanido, fr_halogen, fr_hdrzine, fr_hdrzone, fr_imidazole, fr_imide, fr_isocyan, fr_isothiocyan, fr_ketone,
+ fr_ketone_Topliss, fr_lactam, fr_lactone, fr_methoxy, fr_morpholine, fr_nitrile, fr_nitro, fr_nitro_arom,
+ fr_nitro_arom_nonortho, fr_nitroso, fr_oxazole, fr_oxime, fr_para_hydroxylation, fr_phenol,
+ fr_phenol_noOrthoHbond, fr_phos_acid, fr_phos_ester, fr_piperdine, fr_piperzine, fr_priamide, fr_prisulfonamd,
+ fr_pyridine, fr_quatN, fr_sulfide, fr_sulfonamd, fr_sulfone, fr_term_acetylene, fr_tetrazole, fr_thiazole, fr_thiocyan,
+ fr_thiophene, fr_unbrch_alkane, fr_urea)
+
+
+def calc_rdkit_descriptors(mol):
+    AllChem.ComputeGasteigerCharges(mol)
+    if np.isnan(MinPartialCharge(mol)):
+        print("NaN!", Chem.MolToSmiles(mol))
+        descriptors = None
+    else:
+        descriptors = [
+                BalabanJ(mol) , # 0
+                0.0001*BertzCT(mol) , # 1
+                0.1*Chi0(mol) , # 2
+                0.1*Chi0n(mol) , # 3
+                0.1*Chi0v(mol) , # 4
+                0.1*Chi1(mol) , # 5
+                0.1*Chi1n(mol) , # 6
+                0.1*Chi1v(mol) , # 7
+                0.1*Chi2n(mol) , # 8
+                0.1*Chi2v(mol) , # 9
+                0.1*Chi3n(mol) , # 10
+                0.1*Chi3v(mol) , # 11
+                0.1*Chi4n(mol) , # 12
+                0.1*Chi4v(mol) , # 13
+                0.01*EState_VSA1(mol) , # 14
+                0.01*EState_VSA10(mol) , #15
+                0.01*EState_VSA11(mol) , #16
+                0.01*EState_VSA2(mol) , #17
+                0.01*EState_VSA3(mol) , #18
+                0.01*EState_VSA4(mol) ,
+                0.01*EState_VSA5(mol) , #20
+                0.01*EState_VSA6(mol) ,
+                0.01*EState_VSA7(mol) ,
+                0.01*EState_VSA8(mol) ,
+                0.01*EState_VSA9(mol) ,
+                0.001*ExactMolWt(mol) , #25
+                FractionCSP3(mol) ,
+                HallKierAlpha(mol) ,
+                0.01*HeavyAtomCount(mol) ,
+                0.001*HeavyAtomMolWt(mol) ,
+                0.1*Kappa1(mol) , #30
+                0.1*Kappa2(mol) ,
+                0.001*Kappa3(mol) ,
+                0.01*LabuteASA(mol) ,
+                0.1*MaxAbsEStateIndex(mol) ,
+                MaxAbsPartialCharge(mol) , #35
+                0.1*MaxEStateIndex(mol) ,
+                MaxPartialCharge(mol) , #37
+                MinAbsEStateIndex(mol) ,
+                MinAbsPartialCharge(mol) , #39
+                MinEStateIndex(mol) , #40
+                MinPartialCharge(mol) ,
+                0.1*MolLogP(mol) ,
+                0.01*MolMR(mol) ,
+                0.001*MolWt(mol) ,
+                0.1*NHOHCount(mol) , #45
+                0.1*NOCount(mol) ,
+                NumAliphaticCarbocycles(mol) ,
+                NumAliphaticHeterocycles(mol) ,
+                0.1*NumAliphaticRings(mol) ,
+                NumAromaticCarbocycles(mol) , #50
+                NumAromaticHeterocycles(mol) ,
+                NumAromaticRings(mol) ,
+                0.1*NumHAcceptors(mol) ,
+                0.1*NumHDonors(mol) ,
+                0.1*NumHeteroatoms(mol) , #55
+                NumRadicalElectrons(mol) ,
+                0.1*NumRotatableBonds(mol) ,
+                NumSaturatedCarbocycles(mol) ,
+                NumSaturatedHeterocycles(mol) ,
+                0.1*NumSaturatedRings(mol) , #60
+                0.01*NumValenceElectrons(mol) ,
+                0.01*PEOE_VSA1(mol) ,
+                0.01*PEOE_VSA10(mol) ,
+                0.01*PEOE_VSA11(mol) ,
+                0.01*PEOE_VSA12(mol) , #65
+                0.01*PEOE_VSA13(mol) ,
+                0.01*PEOE_VSA14(mol) ,
+                0.01*PEOE_VSA2(mol) ,
+                0.01*PEOE_VSA3(mol) ,
+                0.01*PEOE_VSA4(mol) , #70
+                0.01*PEOE_VSA5(mol) ,
+                0.01*PEOE_VSA6(mol) ,
+                0.01*PEOE_VSA7(mol) ,
+                0.01*PEOE_VSA8(mol) ,
+                0.01*PEOE_VSA9(mol) , # 75
+                0.1*RingCount(mol) ,
+                0.01*SMR_VSA1(mol) ,
+                0.01*SMR_VSA10(mol) ,
+                0.01*SMR_VSA2(mol) ,
+                0.01*SMR_VSA3(mol) , # 80
+                0.01*SMR_VSA4(mol) ,
+                0.01*SMR_VSA5(mol) ,
+                0.01*SMR_VSA6(mol) ,
+                0.01*SMR_VSA7(mol) ,
+                0.01*SMR_VSA8(mol) , #85
+                0.01*SMR_VSA9(mol) ,
+                0.01*SlogP_VSA1(mol) ,
+                0.01*SlogP_VSA10(mol) ,
+                0.01*SlogP_VSA11(mol) ,
+                0.01*SlogP_VSA12(mol) ,#90
+                0.01*SlogP_VSA2(mol) , #91
+                0.01*SlogP_VSA3(mol) ,
+                0.01*SlogP_VSA4(mol) ,
+                0.01*SlogP_VSA5(mol) ,
+                0.01*SlogP_VSA6(mol) , #95
+                0.01*SlogP_VSA7(mol) ,
+                0.01*SlogP_VSA8(mol) , #97
+                0.01*SlogP_VSA9(mol) ,
+                0.01*TPSA(mol) ,
+                0.01*VSA_EState1(mol) ,#100
+                0.01*VSA_EState10(mol) ,
+                0.01*VSA_EState2(mol) ,
+                0.01*VSA_EState3(mol) ,
+                0.01*VSA_EState4(mol) ,
+                0.01*VSA_EState5(mol) , #105
+                0.01*VSA_EState6(mol) ,
+                0.01*VSA_EState7(mol) ,
+                0.01*VSA_EState8(mol) ,
+                0.01*VSA_EState9(mol) ,
+                fr_Al_COO(mol) , #110
+                fr_Al_OH(mol) ,
+                fr_Al_OH_noTert(mol) ,
+                fr_ArN(mol) ,
+                fr_Ar_COO(mol) ,
+                fr_Ar_N(mol) ,
+                fr_Ar_NH(mol) ,
+                fr_Ar_OH(mol) ,
+                fr_COO(mol) ,
+                fr_COO2(mol) ,
+                fr_C_O(mol) ,
+                fr_C_O_noCOO(mol) ,
+                fr_C_S(mol) ,
+                fr_HOCCN(mol) ,
+                fr_Imine(mol) ,
+                fr_NH0(mol) ,
+                fr_NH1(mol) ,
+                fr_NH2(mol) ,
+                fr_N_O(mol) ,
+                fr_Ndealkylation1(mol) ,
+                fr_Ndealkylation2(mol) ,
+                fr_Nhpyrrole(mol) ,
+                fr_SH(mol) ,
+                fr_aldehyde(mol) ,
+                fr_alkyl_carbamate(mol) ,
+                fr_alkyl_halide(mol) ,
+                fr_allylic_oxid(mol) ,
+                fr_amide(mol) ,
+                fr_amidine(mol) ,
+                fr_aniline(mol) ,
+                fr_aryl_methyl(mol) ,
+                fr_azide(mol) ,
+                fr_azo(mol) ,
+                fr_barbitur(mol) ,
+                fr_benzene(mol) ,
+                fr_benzodiazepine(mol) ,
+                fr_bicyclic(mol) ,
+                fr_diazo(mol) ,
+                fr_dihydropyridine(mol) ,
+                fr_epoxide(mol) ,
+                fr_ester(mol) ,
+                fr_ether(mol) ,
+                fr_furan(mol) ,
+                fr_guanido(mol) ,
+                fr_halogen(mol) ,
+                fr_hdrzine(mol) ,
+                fr_hdrzone(mol) ,
+                fr_imidazole(mol) ,
+                fr_imide(mol) ,
+                fr_isocyan(mol) ,
+                fr_isothiocyan(mol) ,
+                fr_ketone(mol) ,
+                fr_ketone_Topliss(mol) ,
+                fr_lactam(mol) ,
+                fr_lactone(mol) ,
+                fr_methoxy(mol) ,
+                fr_morpholine(mol) ,
+                fr_nitrile(mol) ,
+                fr_nitro(mol) ,
+                fr_nitro_arom(mol) ,
+                fr_nitro_arom_nonortho(mol) ,
+                fr_nitroso(mol) ,
+                fr_oxazole(mol) ,
+                fr_oxime(mol) ,
+                fr_para_hydroxylation(mol) ,
+                fr_phenol(mol) ,
+                fr_phenol_noOrthoHbond(mol) ,
+                fr_phos_acid(mol) ,
+                fr_phos_ester(mol) ,
+                fr_piperdine(mol) ,
+                fr_piperzine(mol) ,
+                fr_priamide(mol) ,
+                fr_prisulfonamd(mol) ,
+                fr_pyridine(mol) ,
+                fr_quatN(mol) ,
+                fr_sulfide(mol) ,
+                fr_sulfonamd(mol) ,
+                fr_sulfone(mol) ,
+                fr_term_acetylene(mol) ,
+                fr_tetrazole(mol) ,
+                fr_thiazole(mol) ,
+                fr_thiocyan(mol) ,
+                fr_thiophene(mol),
+                fr_unbrch_alkane(mol) ,
+                fr_urea(mol) , #rdkit properties # 196
+                ]
+    return descriptors
+
+
+# In[24]:
+
+
+rDKdesc_list = []
+for m in tqdm(mols):
+  desc = calc_rdkit_descriptors(m)
+  rDKdesc_list.append(desc)
+
+
+# In[ ]:
+
+
+desc_df = pd.DataFrame(rDKdesc_list)
+object_dtype = [ idx for idx, i in enumerate(desc_df.dtypes) if i == 'object' ]
+rdk_desc_df = desc_df.drop(desc_df.columns[object_dtype], axis=1)
+
+
+# In[ ]:
+
+
+###dataframe 합치기
+merged_df = pd.concat([score_df, fp_df, rdk_desc_df, maccs_df], axis=1)
+merged_df.to_csv('merged300K_desc.csv', index=False)
