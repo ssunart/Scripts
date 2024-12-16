93d771f72bd0b59f16d32073f12304155024b90d
Soowon Lee <136606810+ssunart@users.noreply.github.com>
2024-12-16 15:18:45 +0900

    Create split_SDF.py

---
+++ split_SDF.py
@@ -0,0 +1,32 @@
+##splitSDF
+import os, sys
+
+def readSDFfile(sdf_f):
+    return open(sdf_f).readlines()
+
+
+def splitMultiSDFtoSingleSDF(sdf_f):
+    multi_lig = readSDFfile(sdf_f)
+    idxList = []
+    name = {}
+    data = []
+    chk = []
+    for idx, i in enumerate(multi_lig):
+        if i.startswith('$$$$'):
+            idxList.append(idx+1)
+
+    
+    for idx, i in enumerate(idxList):
+        if idx == 0:
+            f = open(f"1onp_Lig_{idx}.sdf", "w")
+            for j in multi_lig[0:i]:
+                f.write(f"{j}")
+        else:
+            f = open(f"1onp_Lig_{idx}.sdf", "w")
+            for j in multi_lig[idxList[idx-1]:i]:
+                f.write(f"{j}")
+
+
+
+if __name__ == '__main__':
+    splitMultiSDFtoSingleSDF('1ONP-FOM_Validation_Affinities_3D.sdf')
