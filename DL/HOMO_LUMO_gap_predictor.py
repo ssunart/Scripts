d7a8aa830b4a17b49938a68c1f2270be998568c1
Soowon Lee <136606810+ssunart@users.noreply.github.com>
2024-12-18 15:35:02 +0900

    Update HOMO_LUMO_gap_predictor.py

--- DL/HOMO_LUMO_gap_predictor.py
+++ DL/HOMO_LUMO_gap_predictor.py
@@ -40,3 +40,32 @@ print(os_list)
 print(smi_list[:10])
 
 ###Converting a molecule into an extended circular fingerprint (ECFP)
+from rdkit.Chem import AllChem
+mol = rdkit.Chem.MolFromSmiles(smi_list[0]) # SMILES -> Mol-type 변수로 읽어들인다. 
+vec = AllChem.GetMorganFingerprintAsBitVect(mol, 2, nBits = 1024) # ECFP2를 이용. 1024개의 0/1로 표현하겠다.
+
+smi_list[0]
+print(vec)
+print(vec.ToList())
+
+#RDKit을 이용해서 10만개 분자를 ECFP로 변환
+X_ECFP = []
+for smi in tqdm(smi_list):
+  mol = rdkit.Chem.MolFromSmiles(smi)
+  vec = AllChem.GetMorganFingerprintAsBitVect(mol, 2, nBits = 1024)
+  X_ECFP.append(vec.ToList()) # rdkit 최근 버젼.
+  #X_ECFP.append([int(x) for x in vec.ToBitString()]) # rdkit 구 버젼 (2021 이전 버젼...)
+
+print(len(X_ECFP))
+print(X_ECFP[:2])
+Y = gap_list # 예측하고자하는 목표 물성: HOMO-LUMO gap...
+
+###Spliting training and test set###
+#The ratio of training, validation, and test (holdout) data is a little bit arbitrary.
+#The most common ratios are 6:2:2, 8:1:1, and 7:1:2 etc.
+
+#Scikit-learn 로드
+import sklearn
+from sklearn.model_selection import train_test_split
+
+
