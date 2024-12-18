20874620d7eb9f318d301d34bdc50dcff9eb35ce
Soowon Lee <136606810+ssunart@users.noreply.github.com>
2024-12-18 15:43:31 +0900

    Update HOMO_LUMO_gap_predictor.py

--- DL/HOMO_LUMO_gap_predictor.py
+++ DL/HOMO_LUMO_gap_predictor.py
@@ -68,4 +68,120 @@ Y = gap_list # 예측하고자하는 목표 물성: HOMO-LUMO gap...
 import sklearn
 from sklearn.model_selection import train_test_split
 
+#Train : Test = 8 : 2
+train_X, test_X, train_y, test_y = train_test_split(X_ECFP, Y, test_size = 0.2, shuffle=True)
+print(len(train_X))
+print(len(test_X))
+
+#Training set을 Training / Validation set으로 나눔.
+train_X, val_X, train_y, val_y = train_test_split(train_X, train_y, test_size = 0.1, shuffle=True)
+
+#MLP 모델을 구현
+import torch
+train_X = torch.tensor(train_X, dtype = torch.float32)
+val_X = torch.tensor(val_X, dtype = torch.float32)
+test_X = torch.tensor(test_X, dtype = torch.float32)
+
+train_y = torch.tensor(train_y, dtype = torch.float32)
+val_y = torch.tensor(val_y, dtype = torch.float32)
+test_y = torch.tensor(test_y, dtype = torch.float32)
+
+type(train_X)
+train_X.shape
+
+from torch.utils.data import TensorDataset, DataLoader
+
+ds_train = TensorDataset(train_X, train_y)
+ds_test = TensorDataset(test_X, test_y)
+
+# batch size는 256
+loader_train = DataLoader(ds_train, batch_size=256, shuffle=True, drop_last=True)
+loader_test = DataLoader(ds_test, batch_size=256, shuffle=False)
+
+# 전체 데이터의 개수. 
+print(len(loader_train.dataset))
+print(len(loader_test.dataset))
+
+# 몇 개의 batch가 들어있는지? 
+print(len(loader_train))
+print(len(loader_test))
+
+##Multi-layer perceptron을 이용해서 HOMO-LUMO gap을 예측
+import torch.nn as nn
+
+model = nn.Sequential(
+    nn.Linear(1024, 128), # input_dim = 1024 // output dim = 128
+    nn.LeakyReLU(0.1),    # activation function으로 leakyReLU 사용. https://pytorch.org/docs/stable/nn.html#non-linear-activations-weighted-sum-nonlinearity
+    nn.BatchNorm1d(128),  # Batch Normalization
+    nn.Linear(128, 64),   # 128 dim -> 64 dim
+    nn.LeakyReLU(0.1),    # 
+    nn.BatchNorm1d(64),   
+    nn.Linear(64, 32),    # 64 -> 32 dim
+    nn.LeakyReLU(0.1),
+    nn.BatchNorm1d(32),
+    nn.Linear(32, 1),     # 32 dim -> output_dim = 1 
+    )
+
+print(model)
+
+from torch import optim
+
+# 오차함수 선택 / Selecting a loss function
+loss_fn = nn.MSELoss() # 회귀니까, mean squared error 를 선택. / Since this is a regression task, we will use mean squared error.
+
+# 가중치를 학습하기 위한 최적화 기법 선택 / This is an optimizer to train weight parameters. 
+optimizer = optim.Adam(model.parameters(), lr=0.01, weight_decay = 1e-5)
+
+def train(epoch, X_val, y_val): 
+    loss_list = [] # This list will save loss values 
+    model.train()  # Model is set to a training mode
+
+    # 데이터로더에서 미니배치를 하나씩 꺼내 학습을 수행 // At each step, 256 data points are extracted from the loader.
+    for data, targets in loader_train: 
+
+        optimizer.zero_grad()  # 경사를 0으로 초기화 / All gradients are set to zero.
+        outputs = model(data)  # 데이터를 입력하고 오차 값을 계산 / The predicted values are calculated.
+        outputs = outputs[:, 0] # targets와 모양이 동일하도록. / Convert 2D tensor -> 1D tensor so that it has the same shape with the "targets" tensor.
+
+        loss = loss_fn(outputs, targets)  # loss 값 계산. / Calculating loss value.
+        loss_list.append(loss.item()) # tensor 자체보다, 그 값만 필요할 경우. / Only values are stored. Otherwise, all necessary informations for gradient calculation will be contained in the tensor.
+
+        loss.backward()   # 역전파 계산을 통한 기울기 값 계산 / Calculating gradient values.
+        optimizer.step()  # 역전파 계산한 값으로 가중치를 수정 / Updating gradients with an optimizer.
+
+    val_loss = loss_fn(model(X_val)[:,0], y_val) # calculate validation loss
+    print(f"epoch {epoch} // Loss : {loss:.5f} // Val_Loss : {val_loss:.5f}")
+    
+    return loss.item(), val_loss.item()
+
+###Test set에 대한 성능을 확인하는 함수
+def test():
+    model.eval()  # 신경망을 추론 모드로 전환
+    mse = 0.0
+    # 데이터로더에서 미니배치를 하나씩 꺼내 추론을 수행
+    with torch.no_grad():  # 추론 과정에는 미분이 필요없음
+        for data, targets in loader_test:
+            outputs = model(data)  # 데이터를 입력하고 출력을 계산
+            #print(outputs.shape)
+            #print(targets.shape)
+
+            # 추론 계산
+            mse += torch.sum((outputs[:,0] - targets)**2)
+
+    # 정확도 출력
+    mse /= len(loader_test.dataset)  # 데이터 총 건수
+    #mse = torch.sqrt(mse)
+    print(f'\n테스트 데이터에서 예측 정확도: {torch.sqrt(mse).item():.3f} (eV)')
+
+##100 epoch 동안 학습을 진행
+n_epoch = 100
+train_loss_list = []
+val_loss_list = []
+for epoch in range(n_epoch): # 100 epoch 동안 학습을 시키자!
+    
+    train_loss, val_loss = train(epoch, val_X, val_y)
+    
+    train_loss_list.append(train_loss)
+    val_loss_list.append(val_loss)
+
 
